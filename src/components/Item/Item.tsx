d698a05e0ac4e55a5d9737fb970ee1783354c1ee
Danil Krutikov <kdanil.i@yandex.ru>
2024-07-04 05:21:43 +0700

    Correct interfase IProps

--- src/components/Item/Item.tsx
+++ src/components/Item/Item.tsx
@@ -6,13 +6,12 @@ import { itemProps } from '../../module/interface'
 export const Item = (props: itemProps) => {
   if(props.item.state == 'removed') return
     const { url, MainImage, title, currency_code, price, quantity} = props.item
-    console.log(MainImage);
 
   return (
         <div className="item">
             <div className="item-image">
                 <a href={url}>
-                    <img src={MainImage.url_570xN}/>
+                    <img src={MainImage!.url_570xN}/>
                 </a>
             </div>
             <div className="item-details">
--- src/module/interface.tsx
+++ src/module/interface.tsx
@@ -7,12 +7,12 @@ export interface itemProps{
 }
 
 export interface IProps{
-  listing_id: string,
+  listing_id: number,
   state: string,
-  url: string, 
-  MainImage: {url_570xN: string}, 
-  title: string, 
-  currency_code: string, 
-  price: string, 
-  quantity: string
+  url?: string, 
+  MainImage?: {url_570xN: string}, 
+  title?: string, 
+  currency_code?: string, 
+  price?: string, 
+  quantity?: number | string
 }
